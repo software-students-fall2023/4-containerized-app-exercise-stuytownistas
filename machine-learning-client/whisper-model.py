a9ddebd931f425a7567596b43f99ebfe8a934691
Comeom Rice <cch9399@stern.nyu.edu>
2023-11-20 10:01:28 -0500

    added docker files

--- machine-learning-client/whisper-model.py
+++
@@ -1,9 +0,0 @@
-import ssl
-import whisper
-
-# Disable SSL verification - NOT recommended for anything else! Just a temporary workaround to a bug.
-ssl._create_default_https_context = ssl._create_unverified_context
-
-base_model = whisper.load_model("base")
-result = base_model.transcribe("/Users/wayne/Music/GarageBand/lofi thing.band/Media/Audio Files/Hikaru Nakamura - Stream Highlights-v4a-RY2r7og.wav")
-print(result["text"])
---
+++ machine-learning-client/whisper_model.py
@@ -0,0 +1,6 @@
+import ssl
+import whisper
+
+base_model = whisper.load_model("base")
+result = base_model.transcribe("/Users/wayne/Desktop/Hikaru Takes The Juicer-UfAV4mpJn6A.wav")
+print(result["text"])
