2b6d37c8be9d0dd0899d4982e51d06be85a03c9d
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-22 18:51:28 -0500

    web-app

--- web-app/web_app.py
+++ web-app/web_app.py
@@ -1,30 +1,29 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import whisper
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('webpage.html')
+    transcription = ''
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part', 400
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'No file part', 400
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file', 400
 
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file', 400
+        if file:
+            filename = "uploaded_audio.wav"
+            file.save(filename)
 
-    if file:
-        filename = "uploaded_audio.wav"
-        file.save(filename)
+            # Transcribe using Whisper
+            base_model = whisper.load_model("base")
+            result = base_model.transcribe(filename)
+            transcription = result["text"]
 
-        # Transcribe using Whisper
-        base_model = whisper.load_model("base")
-        result = base_model.transcribe(filename)
-        transcription = result["text"]
-        return jsonify(transcription=transcription)
+    return render_template('webpage.html', transcription=transcription)
 
 if __name__ == '__main__':
     app.run(debug=True)
