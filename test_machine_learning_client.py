876464b21d7f72a5e34bfb20912c58f16b9327d7
James Luo <james@10-17-88-35.dynapool.wireless.nyu.edu>
2023-11-27 12:56:48 -0500

    unit

--- test_machine_learning_client.py
+++ test_machine_learning_client.py
@@ -5,19 +5,19 @@ from whisper_module import base_model
 
 def test_transcribe_success():
     with patch('whisper.load_model', return_value=Mock(transcribe=Mock(return_value={"text": "Test transcription"}))):
-        result = base_model.transcribe("test_audio.wav")
+        result = base_model.transcribe("uploaded_audio.wav")
     
     assert result == {"text": "Test transcription"}
 
 def test_transcribe_failure():
     with patch('whisper.load_model', side_effect=Exception("Model loading failed")):
         with pytest.raises(Exception, match="Model loading failed"):
-            base_model.transcribe("test_audio.wav")
+            base_model.transcribe("uploaded_audio.wav")
 
 def test_main_function():
     with patch('whisper.load_model', return_value=Mock(transcribe=Mock(return_value={"text": "Test transcription"}))):
         with patch('builtins.print') as mock_print:
             with patch('ssl._create_unverified_context', return_value=ssl.SSLContext()):
-                from your_module import main_function
+                from whisper_module import main_function
                 main_function("/path/to/audio/file.wav")
                 mock_print.assert_called_with("Test transcription")
