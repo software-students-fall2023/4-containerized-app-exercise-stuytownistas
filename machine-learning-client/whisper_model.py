c785a6631186b70f9c4ac647507141536ec72c9a
Comeom Rice <cch9399@stern.nyu.edu>
2023-12-02 13:34:02 -0500

    testing stuff

--- machine-learning-client/whisper_model.py
+++ machine-learning-client/whisper_model.py
@@ -1,6 +1,12 @@
 """This module handles the machine learning aspects using Whisper model."""
 import ssl
 import whisper
+import os
+from pymongo import MongoClient
+
+client = MongoClient(os.getenv("MONGO_URI"))
+database = client[os.getenv("MONGO_DATABASE")]
+collection = database[os.getenv("MONGO_COLLECTION")]
 
 base_model = whisper.load_model("base")
 result = base_model.transcribe("/Users/wayne/Desktop/Hikaru Takes The Juicer-UfAV4mpJn6A.wav")
--- web-app/web_app.py
+++ web-app/web_app.py
@@ -1,48 +1,102 @@
 from flask import Flask, request, jsonify, render_template, send_file
 import whisper
-from gridfs import GridFS
+# from gridfs import GridFS
 import io
 from pymongo import MongoClient
+import subprocess
+from bson import Binary
+import os
+from datetime import datetime
 
 # MongoDB URI and Connection
 mongo_uri = "mongodb://mongodb:27017/stuyTownistas"
 client = MongoClient(mongo_uri)
-db = client.get_database('whisper_db')
-fs = GridFS(db)
+db = client["audio"]
+collection = db["audio-collection"]
+# db = client.get_database('whisper_db')
+# fs = GridFS(db)
+print(client.list_database_names())
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    audio_blob = request.files['file']
-    audio_id = fs.put(audio_blob, filename='audio.wav', content_type='audio/wav')
-    return jsonify({'audio_id': str(audio_id)})
+def convert_to_wav(input_data):
+    """
+    Converts an audio file to WAV format using FFmpeg.
 
-@app.route('/get_audio/<audio_id>')
-def get_audio(audio_id):
-    audio_data = fs.get(audio_id)
-    return send_file(io.BytesIO(audio_data.read()), mimetype='audio/wav', as_attachment=True, download_name='audio.wav')
+    Parameters:
+    input_data (bytes): The input audio data.
+    input_format (str): The format of the input audio data.
 
-@app.route('/', methods=['GET', 'POST'])
+    Returns:
+    bytes: The converted audio data in WAV format.
+    """
+    with open("temp_input_file", "wb") as temp_input:
+        temp_input.write(input_data)
+
+    output_file = "temp_output_file.wav"
+    command = [
+        "ffmpeg",
+        "-i",
+        "temp_input_file",
+        "-ar",
+        "44100",
+        "-ac",
+        "2",
+        output_file,
+    ]
+    try:
+        subprocess.run(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while converting the file: ", e)
+        return None
+
+    with open(output_file, "rb") as wav_output:
+        wav_data = wav_output.read()
+
+    os.remove("temp_input_file")
+    os.remove(output_file)
+    return wav_data
+
+
+
+@app.route('/')
 def index():
-    transcription = ''
-    if request.method == 'POST':
-        audio_id = request.form['audio_id']
-        try:
-            audio_data = fs.get(audio_id)
-            with open("uploaded_audio.wav", "wb") as f:
-                f.write(audio_data.read())
+    return render_template('webpage.html')
 
-            # Transcribe using Whisper
-            base_model = whisper.load_model("base")
-            result = base_model.transcribe("uploaded_audio.wav")
-            transcription = result["text"]
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
 
-        except Exception as e:
-            print(e)
-            return 'Error processing the file', 500
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
 
-    return render_template('webpage.html', transcription=transcription)
+    if file:
+        file_data = file.read()
+        wav_file = convert_to_wav(file_data)
+        filename = "test_audio.wav"
+        file.save(filename)
+
+        audio_file = {
+            "name": file.filename,
+            "audio_data": Binary(wav_file),
+            "recorded_date": datetime.utcnow().strftime("%B %d %H:%M:%S"),
+        }
+    
+        # Store file as binary. (This step isn't working right now)
+        collection.insert_one(audio_file)
+
+        # Transcribe using Whisper. (This should be in ml_client file)
+        base_model = whisper.load_model("base")
+        result = base_model.transcribe(file_data)
+        transcription = result["text"]
+
+        print("Transcription:", transcription)
+
+        return jsonify(transcription=transcription)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
