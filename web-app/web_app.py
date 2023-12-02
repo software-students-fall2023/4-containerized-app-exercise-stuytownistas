2e6ef3728d660f7c81273d9353ff2910d06b09c1
xinrantaoangela <xinran.tao2001@gmail.com>
2023-12-02 14:20:24 -0500

    edit dockerfile

--- web-app/web_app.py
+++ web-app/web_app.py
@@ -31,6 +31,38 @@ def get_audio(audio_id):
         print(e)
         return jsonify({'error': str(e)}), 500
 
+#this code shows error of the db
+# @app.route('/transcribe/<audio_id>')
+# def transcribe_audio(audio_id):
+#     try:
+#         audio_data = fs.get(audio_id)
+#         with open("temp_uploaded_audio.wav", "wb") as f:
+#             f.write(audio_data.read())
+
+#         # Transcribe using Whisper
+#         base_model = whisper.load_model("base")
+#         result = base_model.transcribe("temp_uploaded_audio.wav")
+#         transcription = result["text"]
+#         return jsonify({'transcription': transcription})
+#     except Exception as e:
+#         print(e)
+#         return jsonify({'error': str(e)}), 500
+
+@app.route('/transcribe/<audio_id>')
+def transcribe_audio(audio_id):
+    try:
+        audio_data = fs.get(ObjectId(audio_id))
+        with open("temp_uploaded_audio.wav", "wb") as f:
+            f.write(audio_data.read())
+
+        # Transcribe using Whisper
+        base_model = whisper.load_model("base")
+        result = base_model.transcribe("temp_uploaded_audio.wav")
+        transcription = result["text"]
+        return jsonify({'transcription': transcription})
+    except Exception as e:
+        traceback.print_exc()  # This will print the full traceback
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/transcribe/<audio_id>')
 def transcribe_audio(audio_id):
